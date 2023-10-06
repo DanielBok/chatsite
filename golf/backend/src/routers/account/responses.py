4a2c3406d732f446b75078728508b1c3b02b17cd
Daniel Bok <daniel.bok@outlook.com>
2023-10-06 17:56:48 +0800

    change account login response output

--- golf/backend/src/routers/account/responses.py
+++ golf/backend/src/routers/account/responses.py
@@ -7,12 +7,14 @@ __all__ = ['Account']
 
 
 class Account(BaseModel):
+    id: int
     username: str
     first_name: str
     last_name: str
+    is_admin: bool
     token: str
 
     @classmethod
     def from_account(cls, acc: m.Account):
         token = auth_svc.construct_token(acc)
-        return cls(**acc.model_dump(include={'username', 'first_name', 'last_name'}), token=token)
+        return cls(**acc.model_dump(include={'username', 'first_name', 'last_name', 'id', 'is_admin'}), token=token)
