a1f171e1fa955e3df211cd8a8ae0b9cb76d251b3
Daniel Bok <daniel.bok@outlook.com>
2023-10-06 17:56:36 +0800

    add cors middleware for local development

--- golf/backend/src/app.py
+++ golf/backend/src/app.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from pkgutil import iter_modules
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database.connector import pool
 from src.repository.seed import seed_application
@@ -16,22 +17,26 @@ VERSION = '1.0'
 
 
 def create_app():
-    _verify_env_vars_defined()
+    is_debug = os.getenv('DEBUG', '0') == '1'  # if DEBUG env var == 1, then debug mode
+
+    _verify_env_vars_defined(is_debug)
+
     app = FastAPI(title="My Golf Statistics",
                   description="API service to keep track of my golf statistics",
                   version=VERSION,
                   docs_url=None,
                   redoc_url="/docs",
-                  lifespan=lifespan)
+                  lifespan=_lifespan)
 
+    _add_middlewares(app, is_debug)
     _add_routes(app)
 
     return app
 
 
-def _verify_env_vars_defined():
-    if os.getenv('DEBUG', '0') != '1':
-        # only check when in debug mode
+def _verify_env_vars_defined(is_debug: bool):
+    if not is_debug:
+        # only check when in production mode
         keys = {
             'APP_FIRST_USER_USERNAME',
             'APP_FIRST_USER_PASSWORD',
@@ -50,7 +55,7 @@ def _verify_env_vars_defined():
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def _lifespan(app: FastAPI):
     logging.info("Opening ConnectionPool for database access")
     pool.open()
     seed_application()
@@ -58,6 +63,19 @@ async def lifespan(app: FastAPI):
     yield
 
     pool.close()
+
+
+def _add_middlewares(app: FastAPI, is_debug: bool):
+    if is_debug:
+        # allow CORS when debugging
+        origins = ['http://localhost:3000']
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=['*'],
+            allow_headers=['*']
+        )
 
 
 def _add_routes(app: FastAPI):
