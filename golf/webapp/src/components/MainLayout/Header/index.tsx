9f8edaf0754cdcb10f792d357d486b76720b7b1a
Daniel Bok <daniel.bok@outlook.com>
2023-10-07 16:20:44 +0800

    dirty code for nextjs

--- golf/webapp/src/components/MainLayout/Header/index.tsx
+++ golf/webapp/src/components/MainLayout/Header/index.tsx
@@ -1,27 +1,40 @@
+import { getDefaultSession } from "@/app/api/auth/[...nextauth]/route";
 import CatLogo from "@/components/MainLayout/Header/cat-logo.svg";
 import { APP_NAME } from "@/constants";
 import { Col, Row } from "antd";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Header = () => {
+const Header = async () => {
+  const session = await getDefaultSession();
+  console.log(session);
+
   return (
     <div className="h-24 sticky z-10 top-0 bg-teal-600 border-b-gray-400 border-b shadow-xl">
       <Row className="p-4 text-white">
         <Col span={12}>
-          <Row align="middle" justify="start">
+          <Link href="/" className="cursor-pointer flex align-middle">
             <Image src={CatLogo} alt="Cat Logo" height={64} width={64}/>
-            <div className="ml-4 font-bold text-3xl">
+            <div className="ml-4 font-bold text-3xl text-white hover:text-gray-100 flex items-center">
               {APP_NAME}
             </div>
-          </Row>
+          </Link>
         </Col>
         <Col span={12}>
-          <Row justify="end" align="middle">
-            <Col className="h-full">
-              Login
-            </Col>
-          </Row>
+          <div className="flex justify-end items-center h-full">
+            {session?.user ? (
+              <div>
+                Signed in
+              </div>
+            ) : (
+              <Link href="/auth/signin"
+                    className="text-white hover:text-gray-100 text-lg h-full items-center flex">
+                Log in
+              </Link>
+            )
+            }
+          </div>
         </Col>
       </Row>
     </div>
