8052edba9859c7c061cc7d25fb608255d967f05a
Daniel Bok <daniel.bok@outlook.com>
2023-10-09 17:31:16 +0800

    add user image

--- golf/webapp/src/components/MainLayout/Header/AccountHandler.tsx
+++ golf/webapp/src/components/MainLayout/Header/AccountHandler.tsx
@@ -2,10 +2,16 @@ import { useAuth } from "@/context/auth-context";
 import { DownOutlined, UserOutlined, } from "@ant-design/icons";
 import { Dropdown, MenuProps } from "antd";
 import classNames from "classnames";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function AccountHandler() {
   const {user, signOut} = useAuth();
+
+  if (!user) {
+    redirect("/account/login");
+  }
 
   const items: MenuProps["items"] = [
     {
@@ -26,7 +32,12 @@ export default function AccountHandler() {
         "text-bold text-lg cursor-pointer border-b-white border-b p-1 items-center flex",
         "hover:text-gray-300 hover:border-b-gray-300",
       )}>
-        {user!.firstName}
+        {user.imagePath && <Image src={user.imagePath}
+                                  className="mr-2"
+                                  alt=""
+                                  width={32}
+                                  height={32}/>}
+        <div>{user.name}</div>
         <DownOutlined className="ml-1 text-sm"/>
       </div>
     </Dropdown>
