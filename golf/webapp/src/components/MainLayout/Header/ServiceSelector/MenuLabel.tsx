16ffd7d7d6f6974f815479f4dd51225cbb00a48d
Daniel Bok <daniel.bok@outlook.com>
2023-10-10 16:32:17 +0800

    add Links to ServiceSelector

--- golf/webapp/src/components/MainLayout/Header/ServiceSelector/MenuLabel.tsx
+++ golf/webapp/src/components/MainLayout/Header/ServiceSelector/MenuLabel.tsx
@@ -1,25 +1,29 @@
 import classNames from "classnames";
+import Link from "next/link";
 import React from "react";
 
 type Props = {
-  Icon: React.FC;
+  icon: React.ReactNode;
+  linkTo: string
   selected?: boolean;
 }
 
-export default function MenuLabel({children, Icon, selected = false}: React.PropsWithChildren<Props>) {
+export default function MenuLabel({children, icon, linkTo, selected = false}: React.PropsWithChildren<Props>) {
   if (typeof children === "string") {
     children = <span>{children}</span>;
   }
 
   return (
-    <div className={classNames(
-      "text-white flex items-center text-lg",
-      selected
-        ? "border-b-amber-400 border-b-4 text-amber-400"
-        : "hover:text-amber-200 hover:border-b-amber-200 hover:border-b-2"
-    )}>
-      <Icon/>
-      {children}
-    </div>
+    <Link href={linkTo}>
+      <div className={classNames(
+        "text-white flex items-center text-lg",
+        selected
+          ? "border-b-amber-400 border-b-4 text-amber-400"
+          : "hover:text-amber-200 hover:border-b-amber-200 hover:border-b-2"
+      )}>
+        {icon}
+        {children}
+      </div>
+    </Link>
   );
 }
