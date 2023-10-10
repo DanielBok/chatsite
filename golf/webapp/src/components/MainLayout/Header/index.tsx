86193359b57684a2267ba414f5e12be0419e393b
Daniel Bok <daniel.bok@outlook.com>
2023-10-10 15:12:44 +0800

    refactor AccountHandler

--- golf/webapp/src/components/MainLayout/Header/index.tsx
+++ golf/webapp/src/components/MainLayout/Header/index.tsx
@@ -1,48 +1,31 @@
-import AccountHandler from "@/components/MainLayout/Header/AccountHandler";
-import CatLogo from "@/components/MainLayout/Header/cat-logo.png";
 import { APP_NAME } from "@/constants";
-import { useAuth } from "@/context/auth-context";
-import { Col, Row } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import AccountHandler from "./AccountHandler";
+import CatLogo from "./cat-logo.png";
+import ServiceSelector from "./ServiceSelector";
 
 
-const Header = () => {
-  const {loading, user} = useAuth();
-
+export default function Header() {
   return (
     <div className="h-24 sticky z-10 top-0 bg-teal-600 border-b-gray-400 border-b shadow-xl">
-      <Row className="p-4 text-white">
-        <Col span={12}>
+      <div className="p-4 text-white flex">
+        <div>
           <Link href="/" className="cursor-pointer flex align-middle">
             <Image src={CatLogo} alt="Cat Logo" height={64} width={64}/>
             <div className="ml-4 font-bold text-3xl text-white hover:text-gray-100 flex items-center">
               {APP_NAME}
             </div>
           </Link>
-        </Col>
-        <Col span={12}>
-          <div className="flex justify-end items-center h-full">
-            {
-              !loading && <>
-                {
-                  user ? (
-                    <AccountHandler/>
-                  ) : (
-                    <Link href="/account/signin"
-                          className="text-white hover:text-gray-100 text-lg h-full items-center flex">
-                      Log in
-                    </Link>
-                  )
-                }
-              </>
-            }
-          </div>
-        </Col>
-      </Row>
+        </div>
+        <div className="flex-1 ml-8 mr-4 flex items-center">
+          <ServiceSelector/>
+        </div>
+        <div className="flex justify-end items-center h-full">
+          <AccountHandler/>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default Header;
+}
