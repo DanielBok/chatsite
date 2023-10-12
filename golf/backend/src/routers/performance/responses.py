8888a04290baa543c7e1268be61a0928d012e863
Daniel Bok <daniel.bok@outlook.com>
2023-10-12 12:06:17 +0800

    add routes for performance

---
+++ golf/backend/src/routers/performance/responses.py
@@ -0,0 +1,64 @@
+from pydantic import BaseModel
+
+import src.repository.score.models as m
+from src.repository.course.models import Course
+
+__all__ = ['Score']
+
+
+class ScoreDetail(BaseModel):
+    hole: int
+    par: int
+    index: int
+    distance: int
+    score: int
+
+
+class Score(m.Score):
+    id: int
+    tee: m.GolfTeeEnum
+    game: m.GameEnum
+    datetime: m.AwareDatetime
+    metric: str
+    scores: list[ScoreDetail]
+
+    @classmethod
+    def from_components(cls, score: m.Score, course: Course):
+        par = course.par
+        index = course.index
+
+        for tee_info in course.tee_info:
+            if tee_info.tee == score.tee:
+                metric = tee_info.distance_metric
+                distances = tee_info.distance
+                break
+        else:
+            raise ValueError(f"No <{score.tee}> tee information for course id={course.id}")
+
+        start = 1
+        match score.game:
+            case m.GameEnum.Front:
+                par = par[:9]
+                index = index[:9]
+                distances = distances[:9]
+
+            case m.GameEnum.Back:
+                par = par[9:]
+                index = index[9:]
+                distances = distances[9:]
+                start = 10
+
+        if not (len(par) == len(index) == len(distances) == len(score.scores)):
+            raise ValueError(f"Issue with score (id={score.id})")
+
+        return {
+            'id': score.id,
+            'tee': score.tee,
+            'game': score.game,
+            'datetime': score.datetime,
+            'scores': [
+                {'hole': start + x, 'par': p, 'index': i, 'distance': d, 'score': s}
+                for x, (p, i, d, s) in enumerate(zip(par, index, distances, score.scores))
+            ],
+            'metric': metric,
+        }
---
+++ golf/backend/src/routers/performance/router.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, APIRouter
+
+import src.repository.score.models as m
+import src.routers.performance.responses as resp
+from src.repository.account.models import Account
+from src.repository.course.repo import CourseRepository
+from src.repository.score.repo import ScoreRepository
+from src.services.authentication import auth_svc
+
+router = APIRouter(prefix="/performance", tags=["Golf Performance", "Scores", "Course Performance"])
+
+
+@router.get("/", response_model=list[resp.Score])
+def get_score(acc: Account = Depends(auth_svc),
+              score_repo: ScoreRepository = Depends(),
+              course_repo: CourseRepository = Depends()):
+    scores = score_repo.fetch_scores(player_id=acc.id)
+    courses: dict[int, resp.Course] = {
+        c.id: c for c in
+        course_repo.fetch_courses(course_id=[s.course_id for s in scores])
+    }
+
+    return [resp.Score.from_components(score, courses[score.course_id]) for score in scores]
+
+
+@router.post("/", response_model=resp.Score)
+def post_score(new_score: m.CreateScore,
+               acc: Account = Depends(),
+               score_repo: ScoreRepository = Depends(),
+               course_repo: CourseRepository = Depends()):
+    new_score.player_id = acc.id
+    score = score_repo.add_score(new_score)
+    course = course_repo.fetch_course_by_id(score.course_id)
+
+    return resp.Score.from_components(score, course)
