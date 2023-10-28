31dca8689e929e51fee6a2d1cf0479b4178a07a7
Daniel Bok <daniel.bok@outlook.com>
2023-10-28 23:38:51 +0800

    flex column links for mobile devices

--- wedding/webapp/src/app/(components)/PageLinks.tsx
+++ wedding/webapp/src/app/(components)/PageLinks.tsx
@@ -5,17 +5,15 @@ import React from "react";
 
 export default function PageLinks() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="bg-white opacity-50 rounded p-4">
-        {PageSetup.map(x => (
-          <Link
-            href={x.path}
-            key={x.path}
-            className="m-4 p-2 font-bold font-lg underline text-blue-800">
-            {x.desc}
-          </Link>
-        ))}
-      </div>
+    <div className="bg-white opacity-50 rounded p-4 flex items-center justify-center flex-col md:flex-row">
+      {PageSetup.map(x => (
+        <Link
+          href={x.path}
+          key={x.path}
+          className="m-2 p-2 font-bold font-lg underline text-blue-800">
+          {x.desc}
+        </Link>
+      ))}
     </div>
   );
 }
