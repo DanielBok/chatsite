3cd2a8d8bb0d54b91da78618cf5273663d7eab78
Daniel Bok <daniel.bok@outlook.com>
2023-11-21 16:29:29 +0800

    refactor header

--- kitchen/src/components/MainLayout/Header/WebHeader.tsx
+++ kitchen/src/components/MainLayout/Header/WebHeader.tsx
@@ -1,13 +1,69 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { ExperimentOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 
 export default function WebHeader() {
+  const navigate = useNavigate();
+  const {pathname} = useLocation();
+
+  const menuItems: MenuProps["items"] = [
+    {
+      label: "Cookbook",
+      key: "cookbook",
+      icon: <ExperimentOutlined/>,
+      children: [
+        {
+          label: "Recipe List",
+          key: "/cookbook",
+        },
+        {
+          label: "Add Recipe",
+          key: "/cookbook/add",
+        },
+      ]
+    },
+  ];
+
+  const [selected, setSelected] = useState(searchCurrentKey(pathname, menuItems));
+
   return (
-    <div className="flex-1 ml-8 mr-4 items-center hidden md:flex text-lg text-blue-700 font-bold">
-      <Link to="/cookbook" className="hover:text-blue-400">
-        Cookbook
-      </Link>
+    <div className="flex-1 ml-8 mr-4 items-center hidden md:flex flex-row-reverse">
+      <div className="flex flex-row text-md px-12">
+        <Menu
+          onClick={({key}) => {
+            setSelected(key);
+            if (key.startsWith("/")) {
+              navigate(key);
+            }
+          }}
+          selectedKeys={[selected]}
+          mode="horizontal"
+          items={menuItems}/>
+      </div>
     </div>
   );
+}
+
+function searchCurrentKey(pathname: string, menuItems: MenuProps["items"]): string {
+  return menuItems!.reduce((currentKey, item) => {
+    if (currentKey.length > 0) {
+      return currentKey;
+    }
+
+    const key = item!.key as string;
+
+    if (key === pathname) {
+      return key;
+    }
+
+    const children = (item as any).children || [];
+    if (children.length > 0) {
+      return searchCurrentKey(pathname, children);
+    }
+
+    return currentKey;
+  }, "");
 }
--- kitchen/src/components/MainLayout/Header/index.tsx
+++ kitchen/src/components/MainLayout/Header/index.tsx
@@ -2,22 +2,20 @@ import { APP_NAME } from "@/constants";
 import React from "react";
 import { Image } from "antd";
 import { Link } from "react-router-dom";
-import CatLogo from "./cat-logo.jpg";
+import CatLogo from "./cat-logo.webp";
 import WebHeader from "./WebHeader";
 import MobileHeader from "./MobileHeader";
 
 export default function Header() {
   return (
-    <div className="h-24 sticky z-10 top-0 bg-[#fee26d] border-b-[#fee26d] border-b shadow-xl">
+    <div className="h-20 sticky z-10 top-0 bg-white border-b-gray-400 border-b shadow-xl">
       <div className="p-4 flex">
-        <div>
           <Link to="/" className="cursor-pointer flex align-middle">
-            <Image src={CatLogo} alt="Cat Logo" height={64} width={64} preview={false}/>
-            <div className="ml-4 font-bold text-3xl text-gray-700 hover:text-gray-400 items-center hidden md:flex">
+            <Image src={CatLogo} alt="Cat Logo" height={56} width={56} preview={false}/>
+            <div className="ml-2 font-bold text-2xl text-gray-700 hover:text-gray-400 items-center hidden md:flex">
               {APP_NAME}
             </div>
           </Link>
-        </div>
 
         <MobileHeader/>
         <WebHeader/>
